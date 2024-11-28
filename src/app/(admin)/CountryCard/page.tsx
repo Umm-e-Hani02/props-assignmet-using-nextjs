958b8d7a167580aafbdd938202e8baa1fcd1f63a
Umm-e-Hani02 <165858620+Umm-e-Hani02@users.noreply.github.com>
2024-11-28 20:16:16 +0500

    Update page.tsx

--- src/app/(admin)/CountryCard/page.tsx
+++ src/app/(admin)/CountryCard/page.tsx
@@ -1,23 +1,25 @@
-import Link from 'next/link'
+import Link from 'next/link';
 
-type CountryDetail = {
+const CountryDetailProps = ({
+    name,
+    population,
+    capital
+}:{
     name: string;
-    population: number;
-    capital: string;
-}
-
-const CountryCard = ({name,population,capital}: CountryDetail) => {
-  return (
-    <div className="w-max py-8 m-auto font-bold text-white">
-      <div className="text-4xl text-center text-white py-3"> {name}</div>
-      <div className="text-3xl py-4 m-5">Capital  : {capital}</div>
-      <div className="text-3xl py-4">Population : {population}</div>
-
-      <button className="bg-rose-700 mt-10 ml-32 text-lg py-2 px-6 rounded-md duration-100 text-white hover:scale-110 ">
-        <Link href={"/country"}> Back</Link>
-      </button>
+    population:number;
+    capital:string;
+})=>{
+    return(
+        <div className="w-max py-8 m-auto font-bold text-white">
+        <div className="text-4xl text-center text-white py-3">{name}</div>
+        
+        <div className="text-3xl py-4 m-5">Capital: {capital}</div>
+        
+        <div className="text-3xl py-4">Population: {population}</div>
+        
+        <button className="bg-rose-700 mt-10 ml-32 text-lg py-2 px-6 rounded-md duration-100 text-white hover:scale-110">
+            <Link href="/country">Back</Link>
+        </button>
     </div>
-  )
+    )
 }
-
-export default CountryCard
